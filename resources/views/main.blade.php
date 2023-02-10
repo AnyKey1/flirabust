e169c752bdd8e23a191a365a604e1e6cb399ccf7
anykey <anykey@garage1>
2023-02-10 15:16:07 -0500

    Metrika

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -37,6 +37,26 @@
     <meta property="og:url" content="" />
     <meta property="og:updated_time" content="2020-03-15T15:40:24+06:00" />
 </head>
+
+
+<!-- Yandex.Metrika counter -->
+<script type="text/javascript" >
+   (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+      m[i].l=1*new Date();
+      for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+	         k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+		    (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+
+   ym(40814669, "init", {
+        clickmap:true,
+        trackLinks:true,
+        accurateTrackBounce:true
+   });
+</script>
+<noscript><div><img src="https://mc.yandex.ru/watch/40814669" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
+<!-- /Yandex.Metrika counter -->
+
+
 <body>
 <!-- navigation -->
 <header class="navigation fixed-top">
